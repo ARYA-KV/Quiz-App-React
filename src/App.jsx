e1af53e5691bb4e0f36e96958da36e2a8d6017d9
ARYA-KV <aryakv555@gmail.com>
2024-05-25 11:20:14 +0530

    :completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Quiz from './components/Quiz';
+
+
+const App = () => {
+  return (
+    <>
+       <Quiz/>
+    </>
+  )
+}
+
+export default App
